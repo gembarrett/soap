37b1342c2d33ea6581034ca28a7dfbab3bab560c
Gem Barrett <>
2019-03-21 23:22:07 -0400

    First question and first answers showing on the page

--- assets/js/templates/questionsTemplate.js
+++ assets/js/templates/questionsTemplate.js
@@ -1,20 +1,15 @@
 templates.questionsTemplate = function(data){
   var content = `
-      <div id="all_posts" class="">
-          <h2>All questions</h2>
+      <div id="questionContainer" class="">
   `;
   noOfQuestions = data.length;
   for(var i = 0; i < noOfQuestions; i++) {
       var question = data[i];
-      console.log(question);
-      var answers = question.answers;
-      // for (var i = 0; i < answers.length; i++){
-      //
-      // }
+      content = content + `<h3>`+ question.question +`</h3>`;
+      for (var j = 0; j < question.answers.length; j++){
+        content = content + '<input type="' +question.answers[j].type+ ' id="' +question.id+ j+ '-answer" name="' +question.id+ j+ '-el">' + '<label for="' +question.id+ j+ '-el">' +question.answers[j].answerText+ '</label>';
+      }
 
-      content = content + `
-          <h3><a href="`+question.question+`">`+ question.question +`</a></h3>
-      `;
   }
   content = content + '</div>';
 
--- content/questions.js
+++ content/questions.js
@@ -4,9 +4,9 @@ var questions = [
     "required":"true", // hide the Skip button if true
     "id":"q1",
     "answers":[
-      { "type":"checkbox", "answer":"Our staff participate in work-related travel.", "excludes":""},
-      { "type":"checkbox", "answer":"We provide laptops, phones and other devices to our staff.", "excludes":""},
-      { "type":"checkbox", "answer":"We are a remote organisation; we have at least one member of staff who works remotely.", "excludes":""}
+      { "type":"checkbox", "answerText":"Our staff participate in work-related travel.", "excludes":""},
+      { "type":"checkbox", "answerText":"We provide laptops, phones and other devices to our staff.", "excludes":""},
+      { "type":"checkbox", "answerText":"We are a remote organisation; we have at least one member of staff who works remotely.", "excludes":""}
     ],
     "tips":[ // hide the ? info button if these are all empty
       {"relevance":""},
