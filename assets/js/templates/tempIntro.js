b486d1daa2781d0f86a45ab7a422d4d58cbf0c96
Gem Barrett <>
2019-04-03 17:33:10 -0400

    updated intro text to better explain the tool

--- assets/js/templates/tempIntro.js
+++ assets/js/templates/tempIntro.js
@@ -1,12 +1,14 @@
 templates.tempIntro = function(data){
   var content = `
     <div id="intro">
-      <h3>Not quite ready to launch just yet!</h3>
-      <p>However, if you're interested in reading a research report on organisational security policies and their role in civil society organisations then you're in luck!</p>
+      <h5>When it launches, the SOAP tool will help organisations improve their resilience by providing a free, easy-to-use method for building security policies that are thorough, straightforward and realistic.</h5>
+      <p>SOAP will take the form of an online interface that guides the user through a series of questions, culminating in a security policy that is appropriate to their needs and accompanied by an appendix of additional tips and resources for implementation and future maintenance. SOAP can be used as part of a larger security prioritisation plan, for instance to prepare staff for an upcoming security audit.</p>
+      <p>Want to be notified when this tool launches? Please email directly to <a href="mailto:gem@usesoap.app">gem@usesoap.app</a> with the subject line "Notify me!" and you'll receive an email upon launch. If you would also like to be involved in the user testing of this tool prior to launch, please state that in your email.</p>
+      <hr />
+      <p>Although you can't use SOAP yet, if you're interested in reading a research report on organisational security policies and their role in civil society organisations then you're in luck!</p>
       <p>Securing Civil Society is the result of several months of research into the practices, processes and problems involved when civil society organisations consider implementing a security policy. Based on interviews with individuals representing 14 organisations from around the world, this 25-page report examines questions such as:</p>
       <ul><li>What do organisations usually put in their security policy?</li><li>What are the common barriers they face when implementing a policy?</li><li>How do organisations without a policy manage their security?</li></ul>
-      <h6><a class="publicLink" href="assets/SecuringCivilSociety-report.pdf">Get the report</a></h6>
-      <p>Want to be notified when this tool launches? Please email directly to <a href="mailto:gem@usesoap.app">gem@usesoap.app</a> with the subject line "Notify me!" and you'll receive an email upon launch. If you would also like to be involved in the user testing of this tool prior to launch, please state that in your email.</p>
+      <h5><a class="publicLink" href="assets/SecuringCivilSociety-report.pdf">Get the report</a></h5>
     </div>
     `;
     return content;
