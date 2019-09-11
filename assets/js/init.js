7f34a2f919a85023518828eb0f1557cb4f79f067
Gem Barrett <>
2019-09-11 17:23:12 -0400

    fixed #6 so there's now a check for JS

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,7 +4,7 @@ var views = {};
 
 window.onload = function(){
   console.log('Window loaded');
-
+  document.querySelector('#no-js').remove();
   window.addEventListener(
       "hashchange",
       function(){utils.router()}
