0a9bfa0d23c2859069476eb580ce48e0eb664c60
Gem Barrett <>
2019-09-12 15:01:54 -0400

    removed pdf download option for now

--- assets/js/templates/policyTemplate.js
+++ assets/js/templates/policyTemplate.js
@@ -3,9 +3,8 @@ templates.policyTemplate = function(data){
   console.log('your policy is' + policyText);
   // TODO: refactor this
   txt = '<button onclick="download(policyText, \'policyDoc\', \'text/plain\')">Text file</button>';
-  pdf = '<button onclick="download(policyText, \'policyDoc\', \'application/pdf\')">PDF file</button>';
   md = '<button onclick="download(policyText, \'policyDoc\', \'text/markdown\')">Markdown file</button>';
   html = '<button onclick="download(policyText, \'policyDoc\', \'text/html\')">HTML file</button>';
-  var content = '<textarea class="policyHolder">'+policyText+'</textarea>' + txt + pdf + md + html;
+  var content = '<textarea class="policyHolder">'+policyText+'</textarea>' + txt + md + html;
   return content;
 };
