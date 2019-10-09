e7f4d9db7933a6efdd409253c598cd52b00e4e05
Gem Barrett <>
2019-10-09 17:36:03 -0400

    improving the styling and content of the policy page #8

--- assets/js/templates/policyTemplate.js
+++ assets/js/templates/policyTemplate.js
@@ -1,9 +1,9 @@
 templates.policyTemplate = function(data){
   policyText = compilePolicy();
   // TODO: refactor this
-  txt = '<button onclick="downloadPolicy(\'text/plain\')">Text file</button>';
-  md = '<button onclick="downloadPolicy(\'text/markdown\')">Markdown file</button>';
-  html = '<button onclick="downloadPolicy(\'text/html\')">HTML file</button>';
-  var content = '<textarea class="policyHolder">'+policyText+'</textarea>' + txt + md + html;
+  txt = '<button class="btn btn-prim" onclick="downloadPolicy(\'text/plain\')">Text file</button>';
+  md = '<button class="btn btn-prim" onclick="downloadPolicy(\'text/markdown\')">Markdown file</button>';
+  html = '<button class="btn btn-prim" onclick="downloadPolicy(\'text/html\')">HTML file</button>';
+  var content = '<h2>You built a security policy!</h2><p>You are free to edit it in the textbox below, then copy and paste it into your own document. Or simply download the unedited version as text, markdown or html using the buttons.</p><textarea class="policyHolder">'+policyText+'</textarea>' + txt + md + html;
   return content;
 };
