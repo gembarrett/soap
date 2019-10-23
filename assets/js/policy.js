ebb7bc5be8bf23b5c9e26af2c08a2644c9fb1d17
Gem Barrett <>
2019-10-22 22:57:03 -0400

    code needs refactoring but appendix now starting to be organised

--- assets/js/policy.js
+++ assets/js/policy.js
@@ -48,52 +48,71 @@ function compilePolicy() {
 // function that compiles the appendix based on what's in the answer storage at that time
 function compileAppendix() {
   // create local policy variable
-  var tempAppendix = [];
+  var tempGeneral = [];
+  var tempReview = [];
+  var tempTips = [];
+
   // initialise prevQ
   var prevQ = currentState.answers[0].q;
   // for each of the stored answers
   for (var i = 0; i < currentState.answers.length; i++) {
     // grab the question and answer
     qRef = currentState.answers[i].q;
-    aRef = currentState.answers[i].a;
-    // set up current question, general content, specific content and answer text storage
+    aRef = currentState.answers[i].a; // TODO: either use this or delete
+
+    // set up question name
     var thisQ = 'q'+qRef;
-    var answer = "";
-    // check each section for the question with the right id
+    // var answer = "";
+
+    // find the section that has this question
     for (var j = 0; j < sections.length; j++) {
 
       // search for question with correct id
       var found = sections[j].find(ans => ans.id === thisQ);
-      // when it's found, check for policyContent
+      // when it's found, check for appendix content
       if (found) {
-        // ring the bell, we got ourselves a winner!
-
-        // if this question number is not the same as the prevQ
+        // if we haven't already got the general content from this question
         if (qRef != prevQ) {
-          // if general hasn't already been created then create and grab general policy content
           // check the general content isn't empty!
           if (found.appendixContent !== "") {
-            tempAppendix.push(found.appendixContent);
+            // grab the general appendix content for this question
+            tempGeneral.push(found.appendixContent);
           }
         }
-
+        console.log(found);
+        // create an easy reference for the specific appendix content
         var appendix = found.answers[currentState.answers[i].a].appendixEntry[0];
-
+        console.log(appendix);
         // if there's review or tip content
         if (appendix.reviewList.length > 0){
-          tempAppendix.push(appendix.reviewList);
+          tempReview.push(appendix.reviewList);
         }
         if (appendix.tipList.length > 0){
-          tempAppendix.push(appendix.tipList);
+          tempTips.push(appendix.tipList);
         }
       }
     }
     // store this question's ID for comparison in the next loop
     prevQ = qRef;
   }
-  console.log(tempAppendix);
+  console.log(tempGeneral.length, tempReview.length, tempTips.length);
   // replace all temporary words with values from dict
-  return replaceTemp(tempAppendix);
+  tempGeneral = replaceTemp(tempGeneral);
+  tempReview = replaceTemp(tempReview);
+  tempTips = replaceTemp(tempTips);
+
+  tempAppendix = '-----Appendix-----'+'\n'
+  if (tempGeneral.length > 0) {
+    tempAppendix += tempGeneral+'\n';
+  }
+  if (tempReview.length > 0){
+    tempAppendix += '-----Review checklist-----'+'\n'+tempReview+'\n';
+  }
+  if (tempTips.length > 0){
+    tempAppendix += '-----Implementation tips-----'+'\n'+tempTips;
+  }
+  console.log(tempAppendix)
+  return tempAppendix;
 }
 
 // function to replace placeholder text in policy
@@ -120,6 +139,16 @@ function replaceTemp(policyArr) {
   // join all the edited array entries together into a single string
   editedArray = editedArray.join("\n");
   return editedArray;
+}
+
+// takes all the array items and puts them in the right order and groupings
+function formatPolicy(pol){
+
+}
+
+// takes all the review and tip items and organises them
+function formatAppendix(app){
+
 }
 
 // function to download data to a file
