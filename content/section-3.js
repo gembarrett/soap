f173f476e41a09aea0c2800199d495dd66427675
Gem Barrett <>
2019-11-15 13:44:05 -0500

    creating empty template for start of section 3 content #47

---
+++ content/section-3.js
@@ -0,0 +1,67 @@
+var section3 = [
+  {
+    "isQuestion": false,
+    "id":"q20",
+    "title": "Scenario three",
+    "paragraph":["Device confiscation scenario"]
+  },
+  {
+    "isQuestion": true,
+    "id":"q21",
+    "q":"",
+    "required":false,
+    "policyContent":"",
+    "appendixContent":"",
+    "answers":[
+      {
+        "type":"radio",
+        "answerText":"",
+        "storeAs":"[]",
+        "excludes":[],
+        "policyEntry":"",
+        "appendixEntry":[
+          {
+            "reviewList":"",
+            "tipList": ""
+          }
+        ]
+      }
+    ],
+    "tips":[
+      {"relevance":""},
+      {"meaning":""},
+      {"implementation":""},
+      {"more": [""]}
+    ]
+  },
+  {
+    "isQuestion": true,
+    "id":"q22",
+    "q":"Bearing the previous questions in mind, what steps should staff take when their devices are confiscated?",
+    "required":true,
+    "policyContent":"... your device is confiscated: you are advised to follow these steps: [inc3Tactics]",
+    "appendixContent": "",
+    "answers":[
+      {
+        "type":"textarea", // check for this before making an input field
+        "answerText":"",
+        "placeholder":"Here's a few examples: \n\n",
+        "storeAs":"[inc3Tactics]",
+        "excludes":[],
+        "policyEntry":"",
+        "appendixEntry":[
+          {
+            "reviewList":"",
+            "tipList": ""
+          }
+        ]
+      }
+    ],
+    "tips":[
+      {"relevance":""},
+      {"meaning":""},
+      {"implementation":""},
+      {"more": [""]}
+    ]
+  }
+]
