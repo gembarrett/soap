93ecc8baa587b9d914ee259acaa43d5f7c320808
Gem Barrett <gembarrett@users.noreply.github.com>
2020-06-18 18:20:13 -0400

    moved to switch statements

--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -3,20 +3,31 @@ const bodyEl = document.getElementsByTagName('body');
 document.addEventListener('keyup', reactToPress);
 function reactToPress(e){
   elCode = e.code;
-  // open preview = p = 80
-  if (elCode === "KeyP"){
-    console.log('open preview!');
-  }
-  // close preview = esc = 27
-  else if (elCode === "Escape"){
-    console.log('close preview!');
-  }
-  // next question = return = 13
-  else if (elCode === "Enter"){
-    console.log('next question!');
-  }
-  // select button or checkbox = s = 83
-  else if (elCode === "KeyS"){
+  switch (elCode) {
+    // open preview = p = 80
+    // this key won't be relevant on home page
+    case "KeyP":
+    console.log('open this preview!');
+      break;
+    // close preview = esc = 27
+    // this key won't be relevant on home page
+    case "Escape":
+    console.log('close preview or close edit!');
+      break;
+    // submit answer = return = 13
+    case "Enter":
+    console.log('submit answer!');
+      break;
+    // select button or checkbox = s = 83
+    // this key won't be relevant on home page
+    case "KeyS":
     console.log('select this option!');
+      break;
+    // edit answers = e = 69
+    // this key won't be relevant on home page
+    case "KeyE":
+    console.log('edit answers!');
+      break;
+    default:
   }
 }
