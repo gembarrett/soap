9fe28f56cb48f88ef40cdd2cddbf826aff523b45
Gem Barrett <gembarrett@users.noreply.github.com>
2020-06-18 23:13:34 -0400

    buttons work as expected but not when expected

--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -1,33 +1,92 @@
 const bodyEl = document.getElementsByTagName('body');
 // when a key is pressed
 document.addEventListener('keyup', reactToPress);
+
+// when new question is shown, should the focus be moved to the first element rather than leaving it on Next?
+// when the focused element is an input, find out if the label next door isContentEditable = true
+// if it is editable, then decide on how to handle that
+
 function reactToPress(e){
-  elCode = e.code;
-  switch (elCode) {
-    // open preview = p = 80
-    // this key won't be relevant on home page
-    case "KeyP":
-    console.log('open this preview!');
-      break;
-    // close preview = esc = 27
-    // this key won't be relevant on home page
-    case "Escape":
-    console.log('close preview or close edit!');
-      break;
-    // submit answer = return = 13
-    case "Enter":
-    console.log('submit answer!');
-      break;
-    // select button or checkbox = s = 83
-    // this key won't be relevant on home page
-    case "KeyS":
-    console.log('select this option!');
-      break;
-    // edit answers = e = 69
-    // this key won't be relevant on home page
-    case "KeyE":
-    console.log('edit answers!');
-      break;
-    default:
+  var focusEl = whereIsFocus();
+  console.log(focusEl);
+  // before reacting, check whether the response is needed (e.g. is a text field in focus)
+  if (isItInput(focusEl)) {
+    console.log("it's an input");
+    // if it's an input field we don't need preview or edit reactions
+    // if it's next to a contentEditable label then do something with text entry and S to select
+  } else {
+    elCode = e.code;
+    switch (elCode) {
+      // this key won't be relevant on home page
+      case "KeyP":
+        // simulate a click on the preview button
+        var prevBtn = document.querySelector("#previewPolicy");
+        prevBtn.click();
+        // pressing the key again dismisses the overlay
+        break;
+      // this key won't be relevant on home page
+      case "Enter":
+        // simulate a click on the edit button
+        var subBtn = document.querySelector("#submitAnswers");
+        subBtn.click();
+        break;
+      // this key won't be relevant on home page
+      case "KeyS":
+      console.log('select this option!');
+        // get the button currently in focus
+        var inputBtn = whereIsFocus();
+        console.log(inputBtn);
+        // if the returned element is an input element
+        if (inputBtn.type === "radio" || inputBtn.type === "checkbox"){
+          // click it
+          inputBtn.click();
+        }
+        break;
+      // this key won't be relevant on home page
+      case "KeyE":
+        // simulate a click on the edit button
+        var editBtn = document.querySelector("#editBtn");
+        console.log(editBtn);
+        editBtn.click();
+        break;
+      default:
+    }
   }
+}
+
+function isEditEnabled(){
+  if (document.querySelector("#editBtn")){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isPreviewEnabled(){
+  if (document.querySelector("#previewPolicy")){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isItInput(el) {
+  // if it's a radio, checkbox or textarea
+  if (el.type === "radio" || el.type === "checkbox" || el.type === "textarea"){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function whereIsFocus() {
+  var focused_element = null;
+  if (
+      document.hasFocus() &&
+      document.activeElement !== document.body &&
+      document.activeElement !== document.documentElement
+  ) {
+      focused_element = document.activeElement;
+  }
+  return focused_element;
 }
