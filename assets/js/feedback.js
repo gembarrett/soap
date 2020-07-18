84d15e6db14c7d183e6ca59e5a2389c8c8f58b23
Gem Barrett <gembarrett@users.noreply.github.com>
2020-07-18 00:57:03 -0400

    styling feedback button in footer, removing old text link in questions

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,4 +1,8 @@
+var feedbackBtn = document.getElementById('feedbackBtn');
+feedbackBtn.addEventListener('click', getFeedback, false);
+
 function getFeedback(){
+  console.log('get me some feedback!');
   // when this is triggered, display a modal
   // this modal should have 3 options:
     // take a short survey
