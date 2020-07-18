e6f7ab01181296d28426ba70c49597babc2dbec6
Gem Barrett <gembarrett@users.noreply.github.com>
2020-07-18 00:22:25 -0400

    preparing for feedback modal

---
+++ assets/js/feedback.js
@@ -0,0 +1,7 @@
+function getFeedback(){
+  // when this is triggered, display a modal
+  // this modal should have 3 options:
+    // take a short survey
+    // download public key and open new email
+    // raise an issue on GitHub
+}
