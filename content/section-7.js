11f37cf09c382e804f785caa46d1d10a80d1f7ed
Gem Barrett <gembarrett@users.noreply.github.com>
2020-08-23 21:53:54 -0400

    added content basis for subpolicy question

---
+++ content/section-7.js
@@ -0,0 +1,38 @@
+// this section should always be last so update numbers accordingly when new content is added to other sections
+{
+  "isQuestion": false, // not a question but not an intro either, so detect for this
+  "groups": [
+    {
+      "name": "(e.g. Finance team)"
+    },
+    {
+      "name": "(e.g. remote employees)"
+    },
+  ],
+  "contents": [
+    {
+      "name": "device security",
+      "desc":""
+    },
+    {
+      "name": "communications security",
+      "desc":"",
+    },
+    {
+      "name": "accounts security",
+      "desc":"",
+    },
+    {
+      "name": "travel security",
+      "desc":"",
+    },
+    {
+      "name": "environmental security",
+      "desc":"",
+    },
+    {
+      "name": "network security",
+      "desc":"",
+    }
+  ]
+}
