13fda1f1da9a438a8f5034137a68f9b0619a3a9b
Gem Barrett <gembarrett@users.noreply.github.com>
2020-08-26 02:26:18 -0400

    select first team option by default, group new and old radios

--- assets/js/subpolicies.js
+++ assets/js/subpolicies.js
@@ -5,25 +5,24 @@ function buildSubPolicies(data){
   for (var p = 0; p < data.desc.length; p++){
     layout += `<p>`+data.desc[p]+`</p>`;
   }
-  layout += `</details><div id="teamContentCols">
-            <div id="teams"><button id="addTeam">+ add team (up to 10)</button>`;
+  layout += `</details><div id="teamContentCols"><div id="teams">`;
   // add each of the default teams
   for (var e = 0; e < data.teams.length; e++){
-      layout += `<div class="form-el type-radio">
-                <input type="radio" id="`+data.id+`-0`+e+`-answer" name="`+data.id+`-el">
-                <label for="`+data.id+`-0`+e+`-answer" contenteditable="true" class="btn-edit">`+data.teams[e].name+`</label></div>`;
+      layout += `<div class="form-el type-radio">`;
+      layout += e === 0 ? `<input type="radio" checked="true" id="`+data.id+`-0`+e+`-answer" name="`+data.id+`-el">` : `<input type="radio" id="`+data.id+`-0`+e+`-answer" name="`+data.id+`-el">`;
+      layout += `<label for="`+data.id+`-0`+e+`-answer" contenteditable="true" class="btn-edit">`+data.teams[e].name+`</label></div>`;
   }
-  layout += `</div><div id="content">`;
+  layout += `<button id="`+data.id+`-addTeam" class="btn btn-seco">+ add team (up to 10)</button></div><div id="policyAreas">`;
   // add all of the content areas
   for (var t = 0; t < data.areas.length; t++){
     layout += `<div class="form-el type-checkbox">
       <input type="checkbox" id="`+data.id+`-1`+t+`-answer" name="`+data.id+`-el">
-      <label for="`+data.id+`-1`+t+`-answer">`+data.areas[t].name+`</label></div>`;
+      <label for="`+data.id+`-1`+t+`-answer">`+data.areas[t].name+` <span>`+data.areas[t].desc+`</span></label></div>`;
   }
   // close the columns and add the summary
   layout += `</div></div>
   <div id="expectedOutput">
-    <p>What you'll get when you hit Next:
+    <p>Here's what you'll get when you hit Next:
     <ul><li>Organizational Security Policy (full version)</li>
     <li>Appendix (inc. tips, links and Everyday Practices document)</li>
     </ul></p></div>`;
@@ -41,11 +40,11 @@ function addTeam(e){
   if (allEls.length < 10){
     // build another team element
     thingToAdd = `<div class="form-el type-radio">
-          <input type="radio" id="`+lastQid+`-`+allEls.length+`-answer" name="`+lastQid+`-el">
-          <label for="`+lastQid+`-`+allEls.length+`-answer" contenteditable="true" class="btn-edit">New team</label>
+          <input type="radio" id="0`+lastQid+`-`+allEls.length+`-answer" name="`+lastQid+`-el">
+          <label for="0`+lastQid+`-`+allEls.length+`-answer" contenteditable="true" class="btn-edit">New team</label>
         </div>`;
     // then add it to the screen
-    e.target.insertAdjacentHTML('afterend', thingToAdd);
+    e.target.insertAdjacentHTML('beforebegin', thingToAdd);
     // don't do any other buttony stuff
     e.preventDefault();
   } else {
