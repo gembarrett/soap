7e4ee214062a075164e23e99e7a3d881f01a4aa2
Gem Barrett <gembarrett@users.noreply.github.com>
2020-10-01 14:02:02 -0400

    updated headline

--- content/rest-of-site.js
+++ content/rest-of-site.js
@@ -1,6 +1,6 @@
 var ros = [
   {
-    "head":"The free, easy-to-use online tool that helps civil society organizations build better security policies.",
+    "head":"The free and simple online tool that enables civil society organizations to build better security policies.",
     "subhead":"To get started, enter your organization's name (or an alias):",
     "what":[
       {
